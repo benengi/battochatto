4cf1ba041f6d37d4a8e597df146ada147493572e
William Newman <wnewman@catalyte.io>
2019-04-16 12:36:04 -0400

    Progress

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { ChatMessage } from '../models/chat-message.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  user: any;
+  chatMessages: AngularFireList<ChatMessage[]>;
+  chatMessage: ChatMessage;
+  userName: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {
+    this.afAuth.authState.subscribe(auth => {
+      if (auth !== undefined && auth !== null) {
+        this.user = auth;
+      }
+    });
+  }
+
+  public sendMessage(msg: string) {
+    const timestamp = this.getTimeStamp();
+    const email = this.user.email;
+    this.chatMessages = this.getMessages();
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      userName: this.userName,
+      email
+    });
+  }
+
+  getMessages(): AngularFireList<ChatMessage[]> {
+    // query to create message feed binding
+    return this.db.list('messages', {
+      query: {
+        limitToLast: 25,
+        orderByKey: true
+      }
+    });
+  }
+
+  getTimeStamp() {
+    const now = new Date();
+    const date = now.getUTCFullYear() + '/' +
+    (now.getUTCMonth() + 1) + '/' +
+    now.getUTCDate();
+    const time = now.getUTCHours() + ':' +
+    now.getUTCMinutes() + ':' +
+    now.getUTCSeconds();
+
+    return now + ' ' + time;
+  }
 }
